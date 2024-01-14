b676aa77214d48c493491f3d9e07dd46f98c904b
Dishank7 <127612933+Dishank7@users.noreply.github.com>
2024-01-14 11:26:29 +0000

    weather done

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,20 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const data = { temperature: 25, 
+                 conditions: "Sunny" }
+
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+        {
+          (data.temperature > 20 && <p style={{color : "red"}}>{data.conditions} </p>) || (data.temperature < 20 && <p style={{color : "blue"}}>{data.conditions}</p>)
+        }
+      
+
     </div>
   )
 }
